cd3f8077210822bde12293c100f8a584f15d95f6
[mid9coward] <[hieuvu0422@gmail.com]>
2024-07-29 16:43:57 +0700

    updated

--- src/apiService.js
+++ src/apiService.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_API,
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 /**
